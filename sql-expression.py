bf84f89cc3c6ddb65716ced3070998af0a0eef0a
Michiko  Inoue <inouem888@gmail.com>
2024-10-24 13:18:24 +0000

    Add sql-expression.py

---
+++ sql-expression.py
@@ -0,0 +1,54 @@
+from sqlalchemy import (
+  create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
+)
+
+db = create_engine("postgresql:///chinook")
+
+meta = MetaData(db)
+
+artist_table = Table(
+  "Artist", meta,
+  Column("ArtistId", Integer, primary_key=True),
+  Column("Name", String)
+)
+
+album_table = Table(
+  "Album", meta,
+  Column("AlbumId", Integer, primary_key=True),
+  Column("Title", String),
+  Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
+)
+
+track_table = Table(
+  "Track", meta,
+  Column("TrackId", Integer, primary_key=True),
+  Column("Name", String),
+  Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
+  Column("MediaTypeId", Integer, primary_key=False),
+  Column("GenreId", Integer, primary_key=False),
+  Column("Composer", String),
+  Column("Milliseconds", Integer),
+  Column("Bytes", Integer),
+  Column("UnitPrice", Float),
+)
+with db.connect() as connection:
+
+  # select_query = artist_table.select()
+
+  # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+
+  # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+  
+  
+    # Query 4 - select only by 'ArtistId' #51 from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+
+    # Query 5 - select only the albums with 'ArtistId' #51 on the "Album" table
+    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
+
+    # Query 6 - select all tracks where the composer is 'Queen' from the "Track" table
+    select_query = track_table.select().where(track_table.c.Composer == "Queen")
+  
+    results = connection.execute(select_query)
+    for result in results:
+      print(result)
